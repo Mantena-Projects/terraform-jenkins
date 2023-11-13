788ef367ae8ce5fd519d37af9fe1077330ca9a36
Mantena-Projects <127871554+Mantena-Projects@users.noreply.github.com>
2023-11-13 22:36:55 +0000

    Add files via upload

---
+++ boto3.py
@@ -0,0 +1,10 @@
+import boto3
+
+# Read the text file
+file_path = 'output.txt'  # Replace with your file path
+with open(file_path, 'r') as file:
+    lines = file.readlines()
+
+# Iterate over lines in the file
+for line in lines:
+    print(line)
