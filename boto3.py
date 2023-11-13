d4d41eb2da5c622bf3ec49d0ff2f3ca59765148e
Mantena-Projects <127871554+Mantena-Projects@users.noreply.github.com>
2023-11-13 23:39:15 +0000

    Update boto3.py

--- boto3.py
+++ boto3.py
@@ -3,8 +3,10 @@ import boto3
 # Read the text file
 file_path = 'output.txt'  # Replace with your file path
 with open(file_path, 'r') as file:
-    lines = file.readline()
+    lines = file.readlines()
+    line=lines.strip()
+    print(line)
 
 # Iterate over lines in the file
-for line in lines:
-    print(line)
+#for line in lines:
+    #print(line)
