d66178cbb5f04a677744cb06b080619bf2dd4480
Mantena-Projects <127871554+Mantena-Projects@users.noreply.github.com>
2023-11-14 01:10:51 +0000

    Update boto3.py

--- boto3.py
+++ boto3.py
@@ -8,5 +8,15 @@ for line in lines:
     # Use strip to remove leading and trailing whitespaces
     cleaned_line = line.strip()
     print(cleaned_line)
-   
+
+
+if cleaned_line.startswith("):
+        # Split the line to extract the IP address
+        ip_address = cleaned_line.split('"')[1]
+        cleaned_ips.append(ip_address)
+
+# Now, cleaned_ips contains the extracted IP addresses without "public_ip ="
+print("Extracted IP Addresses:")
+for ip in cleaned_ips:
+    print(ip)
     
