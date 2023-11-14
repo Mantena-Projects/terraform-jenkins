d71de51a72b1c69dc4c0336c44fc3fff4f18f0fa
Mantena-Projects <127871554+Mantena-Projects@users.noreply.github.com>
2023-11-14 02:11:35 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,15 +3,32 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.example.private_key_pem
+  filename = "private_key.pem"
+}
+
+resource "aws_key_pair" "example" {
+  key_name   = "example-key"
+  public_key = tls_private_key.example.public_key_openssh
+}
+
 # Define an AWS EC2 instance
 resource "aws_instance" "example" {
   ami           = "ami-06aa3f7caf3a30282"
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.example.key_name
 }
 
 resource "aws_instance" "example1" {
   ami           = "ami-06aa3f7caf3a30282"
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.example.key_name
 }
 
 # Output the public IP address of the created instance
