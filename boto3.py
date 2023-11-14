8a4bf4c61462bb2e77b31114bcc78a9c2c375396
Mantena-Projects <127871554+Mantena-Projects@users.noreply.github.com>
2023-11-14 00:41:25 +0000

    Update boto3.py

--- boto3.py
+++ boto3.py
@@ -1,11 +1,8 @@
-import boto3
-
-# Read the text file
 file_path = 'output.txt'  # Replace with your file path
 with open(file_path, 'r') as file:
     lines = file.readlines()
 
 for line in lines:
     # Use strip to remove leading and trailing whitespaces
-     cleaned_line = line.strip()
-     #print(f"cleaned line is:'{cleaned_line}'")
+    cleaned_line = line.strip()
+    print(f"cleaned line is: '{cleaned_line}'")
