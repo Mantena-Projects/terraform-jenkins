4fb1adf864558e7ab81f980a29d46ebd09c62035
Mantena-Projects <127871554+Mantena-Projects@users.noreply.github.com>
2023-11-14 00:59:48 +0000

    Update boto3.py

--- boto3.py
+++ boto3.py
@@ -2,7 +2,20 @@ file_path = 'output.txt'  # Replace with your file path
 with open(file_path, 'r') as file:
     lines = file.readlines()
 
+cleaned_ips = []
+
 for line in lines:
     # Use strip to remove leading and trailing whitespaces
     cleaned_line = line.strip()
-    print(f"cleaned line is: '{cleaned_line}'")
+    #print(f"cleaned line is: '{cleaned_line}'")
+    # Check if the line starts with "public_ip ="
+    if cleaned_line.startswith('public_ip ='):
+        # Split the line to extract the IP address
+        ip_address = cleaned_line.split('"')[1]
+        cleaned_ips.append(ip_address)
+
+print("Extracted IP Addresses:")
+for ip in cleaned_ips:
+    print(ip)
+
+    
