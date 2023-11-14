737a61eaeca8389e9829041d37bb7dbcec0f5c82
Mantena-Projects <127871554+Mantena-Projects@users.noreply.github.com>
2023-11-14 01:36:56 +0000

    Update boto3.py

--- boto3.py
+++ boto3.py
@@ -1,22 +1,23 @@
+import re
+
 file_path = 'output.txt'  # Replace with your file path
 with open(file_path, 'r') as file:
     lines = file.readlines()
 
-cleaned_ips = []
+pattern = re.compile(r'^public_ip\d* = "(.*)"$')
 
+# Create a list to store matched IP addresses
+matched_ips = []
+
+# Iterate through the lines and match the pattern
 for line in lines:
-    # Use strip to remove leading and trailing whitespaces
-    cleaned_line = line.strip()
-    print(cleaned_line)
+    match = pattern.match(line.strip())
+    if match:
+        # Access the matched IP address using group(1)
+        ip_address = match.group(1)
+        matched_ips.append(ip_address)
 
-
-    if cleaned_line.startswith("public_ip ="):
-        # Split the line to extract the IP address
-        ip_address = cleaned_line.split('"')[1]
-        cleaned_ips.append(ip_address)
-
-# Now, cleaned_ips contains the extracted IP addresses without "public_ip ="
-print("Extracted IP Addresses:")
-for ip in cleaned_ips:
+# Now, matched_ips contains the extracted IP addresses
+print("Matched IP Addresses:")
+for ip in matched_ips:
     print(ip)
-    
