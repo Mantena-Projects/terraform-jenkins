4636f8d91763a63258eca8811d84d6cacf9ce1a2
Mantena-Projects <127871554+Mantena-Projects@users.noreply.github.com>
2023-11-18 02:43:14 +0000

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,12 +1,12 @@
-resource "aws_security_group" "ssh_sg" {
-  name        = "ssh_security_group"
-  description = "Allow SSH traffic"
+resource "aws_security_group" "example_sg" {
+  name        = "example_security_group"
+  description = "Allow all traffic from all IPv4 addresses"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH traffic from any IP address (adjust as needed)
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all inbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
