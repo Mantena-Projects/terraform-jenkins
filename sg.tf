4013681af64cb0b42e5b92edf376b3b4959b8a44
Mantena-Projects <127871554+Mantena-Projects@users.noreply.github.com>
2023-11-18 02:25:06 +0000

    Add files via upload

---
+++ sg.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"  # Change this to your desired AWS region
+}
+
+resource "aws_security_group" "ssh_sg" {
+  name        = "ssh_security_group"
+  description = "Allow SSH traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH traffic from any IP address (adjust as needed)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
